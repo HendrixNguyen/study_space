c6318c7f9ee6f18fa457eae6a4e82753ce5cd6ed
Hendrix <huyndg.itdev@gmail.com>
2022-02-22 09:18:35 +0700

    update node

--- nodejs/src/index.ts
+++ nodejs/src/index.ts
@@ -1,10 +1,12 @@
+import bodyParser from "body-parser";
 import express from "express";
 
 
 const app = express();
 const PORT = 3000;
 
-app.use(PORT)
+app.use(bodyParser)
+app.use(express.urlencoded());
 
 app.listen(PORT || 3000,() => {
   `this server is running on PORT:${PORT}`
