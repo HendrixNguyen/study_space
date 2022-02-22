cf975718269b1f303a64382ec884c1f8eaa791c0
Hendrix <huyndg.itdev@gmail.com>
2022-02-22 20:58:15 +0700

    .

--- javaspring/demo/src/main/java/com/example/demo/controllers/HomeController.java
+++ javaspring/demo/src/main/java/com/example/demo/controllers/HomeController.java
@@ -2,7 +2,13 @@ package com.example.demo.controllers;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
+    @GetMapping("/hello" )
+    public String helloYou(@RequestParam(value = "", defaultValue = "default") String name) {
+        return String.format("Hello %s", name);
+    };
 }
