cb22c10a58aa6366164a904ead68d569bfc869d5
Hendrix Nguyen <huyndg.itdev@gmail.com>
2022-05-02 15:07:46 +0700

    + update

--- nuxt-portfolio/nuxt.config.ts
+++ nuxt-portfolio/nuxt.config.ts
@@ -2,8 +2,19 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  typescript: {
-    typeCheck: true,
-    strict: true
+  buildModules: [
+    '@nuxt/postcss8',
+    // ...
+  ],
+  css: ['@/assets/css/tailwind.css'],
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+          autoprefixer: {},
+        },
+      },
+    },
   },
 })
