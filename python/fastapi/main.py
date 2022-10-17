055c53c610511c30ffa5d32c009cad0e2380d0ed
Hendrix Nguyen <huyndg.itdev@gmail.com>
2022-10-17 17:15:36 +0700

    +add models

--- python/fastapi/main.py
+++ python/fastapi/main.py
@@ -4,11 +4,12 @@ import os
 from fastapi import FastAPI
 import uvicorn
 
-from . import User
+from . import User, Product
 
 app = FastAPI()
 
 PORT = os.environ.get("PORT") or 5000
+
 
 @app.get("/")
 async def read_root():
@@ -19,11 +20,13 @@ async def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+
 @app.get("/user")
 def read_user():
     return User.getUser()
 
+
 if __name__ == "__main__":
     # os.system('"uvicorn main:app --reload"')
     uvicorn.run(app, host="0.0.0.0", port=PORT, reload=True)
-    print(f'Service is running on port {PORT}')
+    print(f"Service is running on port {PORT}")
