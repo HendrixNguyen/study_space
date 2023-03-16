ef605065725ef0c882658111653f08399806769e
Hendrix Nguyen <huyndg.itdev@gmail.com>
2023-03-16 15:11:14 +0700

    [arch] update

--- setup-arch/setup.sh
+++ setup-arch/setup.sh
@@ -20,3 +20,11 @@ fi
 if [ -z $(which nvm) ]; then
 	wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
 fi
+
+if [ -z $(which k3d) ]; then
+	wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+fi
+
+if [ -z $(which kubectl) ]; then
+	yay -Suy kubectl --noconfirm
+fi
